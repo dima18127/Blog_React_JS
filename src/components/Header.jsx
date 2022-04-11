3f1385138037adc8af088c234f94d2f82f8e736d
Dimas <dima18127@yandex.ru>
2022-04-12 02:18:22 +0400

    popstar1

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+return ( <div className='header'>
+    <img src="https://pp.userapi.com/c626422/v626422722/e550/1OqPss0pQeg.jpg" alt="photoTimati" />
+    <span>Pop stars</span>
+            {/* <ul>
+                <li>AQ</li>
+                <li>SD</li>
+                <li>WE</li>
+            </ul> */}
+        </div>
+        
+        )};
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Profile() {
+return (
+<div className='profile'>
+        <div>
+        <img src="https://delight-events.eu/uploads/news/newsitem/2022/01/21/img_5919.jpg"/> 
+        </div>
+        <img src="https://sun1-92.userapi.com/s/v1/if1/pU1aE_b-cELZoMyjAPYUc7I20QBvbmZuDeb4UKAM_A0CCMy5CoEamEihdxpOxjhcxJ7tc8Yn.jpg?size=100x100&quality=96&crop=48,16,1000,1000&ava=1"/>
+    <ul>
+        <div>
+        <a>ava descr</a>
+        </div>
+        <div>
+        <a>My Posts</a>
+        </div>
+        <div>
+        <a>New Post</a>
+        </div>
+
+        <div>
+        Post 1
+        </div>
+
+        <div>
+        Post 2
+        </div>
+    </ul>
+</div>
+)
+}
+
+export default Profile
